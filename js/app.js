317828f53d04c51789520b33748636424758ccc1
Sinan Bolel <sinanbolel@gmail.com>
2016-06-19 23:51:12 -0500

    Initial commit.

---
+++ js/app.js
@@ -0,0 +1,48 @@
+angular.module('ionic-image-upload', ['ionic', 'ngCordova'])
+
+.run(function($rootScope) {
+  ionic.Platform.ready(function() {
+    if(window.cordova && window.cordova.plugins.Keyboard) {
+      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+      cordova.plugins.Keyboard.disableScroll(true);
+    }
+    if(window.StatusBar) {
+      StatusBar.styleDefault();
+    }
+  });
+})
+
+.directive('file', function() {
+  return {
+    restrict: 'AE',
+    scope: {
+      file: '@'
+    },
+    link: function(scope, el, attrs){
+      el.bind('change', function(event){
+        var files = event.target.files;
+        var file = files[0];
+        if(file.size>0){
+          scope.file = file;
+          scope.$parent.file = file;
+        } else {
+          scope.file = {};
+          scope.$parent.file = {};
+        }
+        scope.$apply();
+      });
+    }
+  };
+})
+
+.controller('UploadController', function ($scope){
+  var imageUploader = new ImageUploader();
+  $scope.file = {};
+  $scope.upload = function() {
+    imageUploader.push($scope.file, function(data){
+      console.log('File uploaded Successfully', $scope.file, data);
+      $scope.uploadUri = data.url;
+      $scope.$digest();
+    });
+  };
+})
